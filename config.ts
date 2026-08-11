960b98603500bb8a3dcb95c061cc7cdc6a3b3a06
kreateupcolleges <kreateupcolleges@gmail.com>
2026-08-11 17:08:41 +0530

    Update Semester 2 configuration with new data

--- config.ts
+++ config.ts
@@ -200,39 +200,65 @@ export const BATCHES: BatchConfig[] = [
       // SEMESTER 2 (NEW — SAMPLE DATA, CHANGE SHEET IDs)
       // ============================================================
 
-  //     "2": {
+      "2": {
 
-  //       label: "Semester 2",
+        label: "Semester 2",
 
-  //       internals: ["IP1", "IP2"],
+        internals: ["IP1", "IP2"],
 
-  //       rewardSheets: {
+ rewardSheets: {
+  IP1: {
+    id: "1W0revb6JjZqYHGvKBykvX7GyDx32fJFL72hNrXH5HD0",
+    name: "AVS_2025-2029_All_S2_IP1_RewardSplit"
+  },
 
-  //         IP1: {
-  //           id: "1cByligzZTzKQQgJA1KTkiEHT1ggeFLibdoFJdHXI4Bw",
-  //           name: "Semester2_IP1_Rewards"
-  //         },
+  /*IP2: {
+    id: "PASTE_SEM2_IP2_REWARD_SHEET_ID",
+    name: "Semester2_IP2_Rewards"
+  }*/
+},
 
-  //         IP2: {
-  //           id: "PASTE_SEM2_IP2_REWARD_SHEET_ID",
-  //           name: "Semester2_IP2_Rewards"
-  //         }
-
-  //       },
-
-
-  //       internalMarksSheets: {
-
-  //       IP1: {
-  //           "B.Sc  AIML": { id: "1fX3C0pqWoJ1NtxpH2TT8kRCrJwOZnQ1IumxifGgnr8Q", name: "B.Sc  AIML" },
-  //           "B.Sc  CS with AI": { id: "14a6Wad24n2yGvuuJE1W8qMkvKqzbG9C24dGrnmaJZMI", name: "B.Sc  CS with AI" },
-  //           "B.Sc CS": { id: "134Xxcovbetu9Ftp0byVoHlcVUNlz6t6OKpnbKI1ISHw", name: "B.Sc CS" },
-  //           "B.Sc DCFS": { id: "1j9EYqC52mTdVymkuSpphqVJJ_Tb1xFVwc2U2oBO_mLY", name: "B.Sc DCFS" },
-  //           "B.Sc DS": { id: "1iWG670KusT54z5nj_UoWldhsm9XJGmf4w-rmjPwEfoc", name: "B.Sc DS" },
-  //           "B.Sc DSA": { id: "1toZUWr1dfEknSrWIl65FLyV24VnXs5nzXVFQJBHs9VY", name: "B.Sc DSA" },
-  //           "B.Sc IT": { id: "1yFS1h8Xd3c0JozHspL81sDdj6iIm0yriqCg0FewlteM", name: "B.Sc IT" }
-  //         },
-
+internalMarksSheets: {
+  IP1: {
+    "AI&ML": {
+      id: "1MmP_uwNpzLQdcefau2Gi90H6TunIb91QAJfLtv-znzM",
+      name: "AI&ML"
+    },
+    "CSE": {
+      id: "1b0yCrYskT01B8a1uIyDIAc6AWqILV-0m5e846RENtrs",
+      name: "CSE"
+    },
+    "AI&DS": {
+      id: "1Sr-cXu0NObfhD_AtKAjk0cdLIMZah1WvD6QAoVRCGLE",
+      name: "AI&DS"
+    },
+    "BME": {
+      id: "1ydFoERh8XFsQOU2Qi6eV_FdRsdsc3d0z8jI50bF1dFo",
+      name: "BME"
+    },
+    "IT": {
+      id: "1nj1h-G64fqSkVd4Lr7W9IgOnqWYyplUbXjcyjaAVJ4U",
+      name: "IT"
+    },
+    "EEE": {
+      id: "1ExmZQk28k0jsl8lH6IRBSOXkCFuKANCcI-OPPu6q28I",
+      name: "EEE"
+    },
+    "CIVIL": {
+      id: "1FyFi84YSKHGHHxBE0rEd_LhCJWeNcwXj3VXSd3QK-SI",
+      name: "CIVIL"
+    },
+    "ECE": {
+      id: "117ObwLKacCAweRJc-cW8bGDXKyvvT8vPnW58ypklg4M",
+      name: "ECE"
+    },
+    "MECH": {
+      id: "14A33H7s8SNZgjRnfvgESCrAzK6K7R-4puQbaAlgQTzw",
+      name: "MECH"
+    }
+    
+  }
+},
 
   //         IP2: {
 
@@ -251,81 +277,118 @@ export const BATCHES: BatchConfig[] = [
   //       },
 
 
-  //       subjectConfig: {
+       subjectConfig: {
 
-  //         defaultMaxMarks: {
-  //           Theory: 15,
-  //           Lab: 15,
-  //           "Lab + Theory": 15
-  //         },
+         defaultMaxMarks: {
+           Theory: 20,
+           Lab: 20,
+          "Lab + Theory": 20
+        },
 
-  //         departments: {
+         departments: {
 
-  //           "B.Sc CS": [
-  //   { code: "25BCS2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BCS2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ],
+          "MECH": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C05", type: "Theory" },
+  { code: "CY25C03", type: "Theory" },
+  { code: "ME25C02", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  // "B.Sc  CS with AI": [
-  //   { code: "25BAR2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BAR2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BAR2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BAR2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ],
+"BME": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "ME25C02", type: "Theory" },
+  { code: "PH25C07", type: "Theory" },
+  { code: "BM25C01", type: "Lab+Theory" },
+  { code: "BM25C02", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  // "B.Sc  AIML": [
-  //   { code: "25BAM2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BAM2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BAM2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BAM2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ],
+"CIVIL": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C02", type: "Theory" },
+  { code: "CY25C02", type: "Theory" },
+  { code: "ME25C02", type: "Theory" },
+  { code: "CE25201", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  // "B.Sc DSA": [
-  //   { code: "25BDA2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDA2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BDA2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDA2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ],
+"AI&DS": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C03", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "CS25C06", type: "Theory" },
+  { code: "AD25201", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  // "B.Sc DS": [
-  //   { code: "25BDS2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDS2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BDS2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDS2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ],
+"CSE": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C03", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "CS25C06", type: "Theory" },
+  { code: "CS25C07", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  // "B.Sc IT": [
-  //   { code: "25BIT2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BIT2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BIT2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BIT2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-    
-  // ],
-  // "B.Sc DCFS": [
-  //   { code: "25BDC2CA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDC2CP", type: "Lab", maxMarks: 15 },
-  //   { code: "25BDC2AA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BDC2EA", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS21T", type: "Theory", maxMarks: 15 },
-  //   { code: "25BCS22E", type: "Theory", maxMarks: 15 }
-  // ]
-  //         }
+"ECE": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "EC25C01", type: "Theory" },
+  { code: "EC25C02", type: "Theory" },
+  { code: "EC25C03", type: "Lab" },
+  { code: "CS25C05", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
 
-  //       }
+"EEE": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C03", type: "Theory" },
+  { code: "PH25C04", type: "Theory" },
+  { code: "ME25C01", type: "Lab+Theory" },
+  { code: "CS25C04", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" },
+  { code: "GE25C01", type: "Theory" }
+],
+
+"IT": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C02", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "IT25201", type: "Lab+Theory" },
+  { code: "IT25202", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+],
+
+"AI&ML": [
+  { code: "UC25H02", type: "Theory" },
+  { code: "EN25C02", type: "Lab+Theory" },
+  { code: "MA25C02", type: "Theory" },
+  { code: "PH25C03", type: "Theory" },
+  { code: "EE25C01", type: "Theory" },
+  { code: "CS25C06", type: "Theory" },
+  { code: "CS25C07", type: "Lab+Theory" },
+  { code: "ME25C05", type: "Lab" }
+]
+          }
+
+     
 
 
   // ============================================================================
@@ -466,6 +529,132 @@ export const BATCHES: BatchConfig[] = [
   //   { code: "CE3481", type: "Lab", maxMarks: 20 },
   //   { code: "ME3461", type: "Lab", maxMarks: 20 }
   // ]
+          }
+        }
+      }
+    }
+  }
+    "2": { 
+        label: "Semester 4", 
+        internals: ["IP1", "IP2"],
+        
+        // Reward Sheets for 2nd Year (using dummy IDs from 1st year for demo)
+        rewardSheets: {
+          "IP1": { id: "1KJXzVhNVbrz9_2CuKS5-px-NmKVTMCFPF09TuZRMogg", name: "AVS_2024-2028_All_S4_IP1_RewardSplit" }
+          // "IP2": { id: "1cJc1Vc2PSR4jUnjzHA", name: "RCS_1styear_IP2_RewardsSplit" }
+        },
+        
+        // Internal Marks for 2nd Year
+        internalMarksSheets: {
+          IP1: {
+             "MECH": { id: "17ZOzCvPve7FwkfedX9Mc2NIIFirEpWbqB2JEQs2MzFk", name: "MECH" },
+             "BME": { id: "1usztgxeLA80WvG66c__R9shytxSXSr6IdsyE5e4edaQ", name: "BME" },
+             "CIVIL ENGINEERING": { id: "1WqpUIh5sbbM0S8cvIbYf4aQe-9ic8t3LNT15lIpeOdI", name: "CIVIL ENGINEERING" },
+             "AI&DS": { id: "17ZOzCvPve7FwkfedX9Mc2NIIFirEpWbqB2JEQs2MzFk", name: "AI&DS" },
+             "CSE": { id: "14tAZVlviH1y7CzynjqkDeI1Wex6o80rUPfFnF7rog-k", name: "CSE" },
+             "ECE": { id: "193xlpfKDF7_yDlPcJ_nr6gtWQhWBOb4liTaOmxEr0hI", name: "ECE" },
+             "EEE": { id: "1-8gCkbQN-wmxr1NvWunfEW65JJw7UiJY8Mz9z4RyDCo", name: "EEE" },
+             "IT": { id: "19mEAWIR_6hppQlAcgjgGLgy9vKfmOT0igAor8k_14YY", name: "IT" }
+          }
+        },
+
+        // Subjects for 2nd Year, Semester 3
+        subjectConfig: {
+          defaultMaxMarks: {
+            Theory: 20,
+            Lab: 20,
+            "Lab+Theory": 20
+          },
+          departments: {
+"BME": [
+    { code: "MA3355", type: "Theory" },
+    { code: "BM3491", type: "Theory" },
+    { code: "BM3402", type: "Theory" },
+    { code: "BM3451", type: "Theory" },
+    { code: "BM3401", type: "Lab+Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "BM3411", type: "Lab" },
+    { code: "BM3412", type: "Lab" }
+  ],
+
+  "ECE": [
+    { code: "EC3451", type: "Theory" },
+    { code: "EC3461", type: "Theory" },
+    { code: "EC3492", type: "Lab+Theory" },
+    { code: "EC3401", type: "Theory" },
+    { code: "EC3452", type: "Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "EC3462", type: "Lab" },
+    { code: "EC3461", type: "Lab" }
+  ],
+
+  "CSE": [
+    { code: "CS3452", type: "Theory" },
+    { code: "CS3491", type: "Lab+Theory" },
+    { code: "CS3492", type: "Theory" },
+    { code: "CS3401", type: "Lab+Theory" },
+    { code: "CS3451", type: "Lab+Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "CS3461", type: "Lab" },
+    { code: "CS3481", type: "Lab" }
+  ],
+
+  "AI&DS": [
+    { code: "CS3591", type: "Lab+Theory" },
+    { code: "AL3452", type: "Lab+Theory" },
+    { code: "AL3451", type: "Theory" },
+    { code: "AD3491", type: "Theory" },
+    { code: "MA3391", type: "Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "AD3411", type: "Lab" },
+    { code: "AL3461", type: "Lab" }
+  ],
+
+  "CIVIL ENGINEERING": [
+    { code: "CE3401", type: "Theory" },
+    { code: "CE3402", type: "Theory" },
+    { code: "CE3403", type: "Theory" },
+    { code: "CE3404", type: "Theory" },
+    { code: "CE3405", type: "Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "CE3411", type: "Lab" },
+    { code: "CE3412", type: "Lab" },
+    { code: "CE3413", type: "Lab" }
+  ],
+
+  "EEE": [
+    { code: "GE3451", type: "Theory" },
+    { code: "EE3401", type: "Theory" },
+    { code: "EE3402", type: "Theory" },
+    { code: "EE3403", type: "Theory" },
+    { code: "EE3404", type: "Theory" },
+    { code: "EE3405", type: "Theory" },
+    { code: "EE3411", type: "Lab" },
+    { code: "EE3412", type: "Lab" },
+    { code: "EE3413", type: "Lab" }
+  ],
+
+  "IT": [
+    { code: "CS3452", type: "Theory" },
+    { code: "CS3453", type: "Lab+Theory" },
+    { code: "CS3454", type: "Theory" },
+    { code: "CS3455", type: "Lab+Theory" },
+    { code: "CS3456", type: "Theory" },
+    { code: "CS3457", type: "Theory" },
+    { code: "CS3458", type: "Lab" },
+    { code: "CS3459", type: "Lab" }
+  ],
+
+  "MECHANICAL": [
+    { code: "ME3491", type: "Theory" },
+    { code: "ME3451", type: "Theory" },
+    { code: "ME3492", type: "Theory" },
+    { code: "ME3493", type: "Theory" },
+    { code: "CE3491", type: "Theory" },
+    { code: "GE3451", type: "Theory" },
+    { code: "CE3481", type: "Lab" },
+    { code: "ME3461", type: "Lab" }
+  ]
           }
         }
       }
